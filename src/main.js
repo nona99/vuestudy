91b69f836e523e56f880c101fb131d4ffb5d124c
DESKTOP-IFT4I94\nona3 <nona3274@gmail.com>
2022-07-31 16:18:22 +0900

    router 설정

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import routes from './routes'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(routes);
+app.mount('#app');
